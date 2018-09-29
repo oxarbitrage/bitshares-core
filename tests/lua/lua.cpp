9bb65ba3b723ec78a9116e295d91e27f3b34ae03
Alfredo <oxarbitrage@gmail.com>
2018-09-29 17:08:35 -0300

    add script send to several accounts from array

--- tests/lua/lua.cpp
+++ tests/lua/lua.cpp
@@ -36,16 +36,16 @@ BOOST_FIXTURE_TEST_SUITE( lua_tests, database_fixture )
 
    BOOST_AUTO_TEST_CASE(lua_test1) {
       try {
-         ACTORS( (dan)(bob)(other) )
+         ACTORS( (dan)(bob)(dreceiver1)(dreceiver2)(dreceiver3) )
 
          create_bitasset("USD", account_id_type());
          transfer(committee_account, dan_id, asset(100000));
          transfer(committee_account, bob_id, asset(200000));
 
+         //uint32_t skip = database::skip_authority_check | database::skip_fork_db | database::skip_validate;
 
-         uint32_t skip = database::skip_authority_check | database::skip_fork_db | database::skip_validate;
-
-         generate_block( skip );
+         //generate_block( skip );
+         generate_block( );
 
          // script 1 - get me block number on each block
          std::string script1 = R"(
@@ -61,12 +61,12 @@ BOOST_FIXTURE_TEST_SUITE( lua_tests, database_fixture )
             sco.output = "";
             sco.status = true;
          });
-         generate_block( skip );
-         generate_block( skip );
-         generate_block( skip );
-         generate_block( skip );
-         generate_block( skip );
-         generate_block( skip );
+         generate_block(  );
+         generate_block(  );
+         generate_block(  );
+         generate_block(  );
+         generate_block(  );
+         generate_block(  );
 
          // script 2 - get me block number, my balance and get out
          std::string script2 = R"(
@@ -85,10 +85,10 @@ BOOST_FIXTURE_TEST_SUITE( lua_tests, database_fixture )
             sco.status = true;
          });
 
-         generate_block( skip );
-         generate_block( skip );
-         generate_block( skip );
-         generate_block( skip );
+         generate_block(  );
+         generate_block(  );
+         generate_block(  );
+         generate_block(  );
 
 
          // script 3 - get me block number, my balance, execute transfer in specified block then just get my balance
@@ -113,9 +113,61 @@ BOOST_FIXTURE_TEST_SUITE( lua_tests, database_fixture )
             sco.status = true;
          });
 
-         generate_block( skip );
-         generate_block( skip );
-         generate_block( skip );
+         generate_block(  );
+
+         auto dan_balance = db.get_balance( dan_id, asset_id_type() );
+         BOOST_CHECK_EQUAL(dan_balance.amount.value, 999900);
+         wdump((dan_balance));
+
+         generate_block(  );
+         generate_block(  );
+
+         // script 4 - pay dividends to a group of accounts
+         std::string script4 = R"(
+         print("---- script 4")
+         receivers = {}
+         receivers[1] = "dreceiver1"
+         receivers[2] = "dreceiver2"
+         receivers[3] = "dreceiver3"
+
+         block_num = Bitshares:getCurrentBlock()
+         print (block_num)
+
+         if block_num == 16 then
+
+            for k,v in ipairs(receivers) do
+                 -- print(v)
+                 Bitshares:transfer("dan", v, "100", "1.3.0")
+            end
+         elseif block_num > 16 then
+            Bitshares:quit()
+         end
+         )";
+
+
+         created = db.create<graphene::lua::smart_contract_object>( [&]( graphene::lua::smart_contract_object& sco ) {
+            sco.owner = dan_id;
+            sco.private_key = dan_private_key;
+            sco.script = script4;
+            sco.output = "";
+            sco.status = true;
+         });
+
+         generate_block(  );
+
+         dan_balance = db.get_balance( dan_id, asset_id_type() );
+         BOOST_CHECK_EQUAL(dan_balance.amount.value, 999600);
+
+         auto dreceiver1_balance = db.get_balance( dreceiver1_id, asset_id_type() );
+         auto dreceiver2_balance = db.get_balance( dreceiver2_id, asset_id_type() );
+         auto dreceiver3_balance = db.get_balance( dreceiver3_id, asset_id_type() );
+         BOOST_CHECK_EQUAL(dreceiver1_balance.amount.value, 100);
+         BOOST_CHECK_EQUAL(dreceiver2_balance.amount.value, 100);
+         BOOST_CHECK_EQUAL(dreceiver3_balance.amount.value, 100);
+
+         generate_block(  );
+
+         generate_block(  );
 
       }
       catch (fc::exception &e) {
